43c48ae81f4bc42e56b0532d548b3c6582d772e0
Annonnymmousss <jatin02012006@gmail.com>
2025-11-28 11:38:54 +0530

    feat : take user input

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+fn main() {
+    // TODO: Uncomment the code below to pass the first stage
+    print!("$ ");
+    io::stdout().flush().unwrap();
+
+    let mut command = String :: new();
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Unable to read the input");
+
+    println!("{}: command not found",command.trim());
+}
